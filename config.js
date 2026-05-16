a5f0bfb955deec6c5beb93dac1fa75e664969e17
rjangrave <rjangrave@users.noreply.github.com>
2026-05-16 21:32:54 +0000

    deploy: d09da76987daba8e3a9f2473b7c6097d0fc52ea3

---
+++ config.js
@@ -0,0 +1,3 @@
+const SUPA_URL  = 'https://yhpatgfyinrbzffkebac.supabase.co';
+const SUPA_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlocGF0Z2Z5aW5yYnpmZmtlYmFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MTE3MDcsImV4cCI6MjA5NDE4NzcwN30.ioYq43E7yn0WZPwJW84zEO9308cXQJi6_j-T6YoQqrY';
+const EDGE_URL  = 'https://yhpatgfyinrbzffkebac.supabase.co/functions/v1/write-record';
